91007d99f1c26b8af5ccfece5736c4e8cb077abc
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-11-16 16:49:24 +0600

    CRUD

--- IntroEF/IntroEF/Controllers/StudentController.cs
+++ IntroEF/IntroEF/Controllers/StudentController.cs
@@ -11,20 +11,55 @@ namespace IntroEF.Controllers
     {
         Fall25_BEntities db = new Fall25_BEntities();
         // GET: Student
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Create() {
+            return View(new Student());
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
         {
-            var s = new Student() { 
-                Name = "S1",
-                Email = "s.a@g.c",
-                Address ="Dhaka"
-            };
+            
             db.Students.Add(s);
             db.SaveChanges();
-            return View();
+            TempData["Msg"] = "Data " + s.Name + " Created";
+            return RedirectToAction("List");
         }
-        public ActionResult List() {
+        public ActionResult List(string search) {
+            if (search != null) { 
+                var list = (from s in db.Students
+                           where s.Name.Contains(search)
+                           || s.Email.Contains(search)
+                           select s).ToList();
+                return View(list);
+            }
             var data = db.Students.ToList();
             return View(data);
         }
+        public ActionResult Details(int id) {
+            var data = db.Students.Find(id); //finds with primary key
+
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            var data = db.Students.Find(id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s) {
+            var dbObj = db.Students.Find(s.Id);
+            //s.Cgpa = dbObj.Cgpa;
+            //db.Students.Remove(dbObj);
+            //db.SaveChanges();
+            db.Entry(dbObj).CurrentValues.SetValues(s);
+            /*dbObj.Email = s.Email;
+            dbObj.Name = s.Name;
+            dbObj.Address = s.Address;*/
+            db.SaveChanges(); 
+            TempData["Msg"] = "Student Updated";
+            return RedirectToAction("List");
+           
+        }
+
     }
 }
